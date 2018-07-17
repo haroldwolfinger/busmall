deca47610ccaf4d41d525df5ccc2279fa02c1e51
Harold Wolfinger <harold.b.wolfinger@protonmail.com>
2018-07-17 16:49:08 -0400

    Revised README.md file and minor edits to main.js file to allow code to run (removed/added semicolons and brackets)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# busmall
+# BusMall Project
 Project No. 3 for Code Partners 201 class
+
+## Project Background and Problem Domain
+BusMall is conducting Focus Group Research on which products to feature in its catalogue. I developed an app that displays three possible candidates at a time and requests the user to click on their favorite item.  This data (the amount of times an image is shown, as well as the number of times it is clicked or not clicked) is stored until the end of the focus session, when that data is summarized and listed for the user.
+
+The project is built on JavaScript, and was scaffolded first before testing.  It showcases a constructor function that creates object instances for each potential product, as well as event handlers and event listeners.  The project also features some if else statements to ensure that during the testing that the same image is not duplicated, and also that images shown in one click test are excluded from being repeated for one turn.
+
+### Stretch Goals For This Lab
+- Handle the display and voting for an arbitrary number of images
+- Using a variable, declare in JavaScript how many images to show
+- Based on that value, dynamically create that many <img> tags
+- Also based on that value, ensure that your randomizer is properly handling the specified number of images for display and repeat tracking
--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 // Scaffolding for the 1st milestone of Sprint 3 (BusMall)
 
 // declare empty array that will store image objects
-Let imageArray = [] ;
+let imageArray = [];
 
 //find image tags in HTML so we can change the SRC attribute
-Let elImage1 = document.getElementByID(‘random1’) ;
-Let elImage2 = document.getElementByID(‘random2’) ;
-Let elImage3 = document.getElementByID(‘random3’) ;
+let elImage1 = document.getElementByID('random1');
+let elImage2 = document.getElementByID('random2');
+let elImage3 = document.getElementByID('random3');
 
 // declare the image index so we have global access
-Let imageIndexArray = [] ;
+let imageIndexArray = [];
 
 // new Constructor for images
-Let Image = (name, filePath, clickYes, clickNo, totalClicks)
+let Image = (name, filePath, clickYes, clickNo, totalClicks)
 	this.name = name ; 
     this.filePath = filePath ; 
     this.clickYes = clickYes ;
@@ -20,74 +20,70 @@ Let Image = (name, filePath, clickYes, clickNo, totalClicks)
     this.totalClicks = totalClicks ;
 
 // instantiate new Images
-Let image01 = ('bag', './img/bag.jpg', 0, 0, 0) ; 
-Let image02 = ('banana', './img/banana.jpg', 0, 0, 0) ; 
-Let image03 = ('bathroom', './img/bathroom.jpg', 0, 0, 0) ;
-Let image04 = ('boots', './img/boots.jpg', 0, 0, 0) ;
-Let image05 = ('breakfast', './img/breakfast.jpg', 0, 0, 0) ;
-Let image06 = ('bubblegum', './img/bubblegum.jpg', 0, 0, 0) ;
-Let image07 = ('chair', './img/chair.jpg', 0, 0, 0) ;
-Let image08 = ('cthulhu', './img/cthulhu.jpg', 0, 0, 0) ;
-Let image09 = ('dog-duck', './img/dog-duck.jpg', 0, 0, 0) ;
-Let image10 = ('dragon', './img/dragon.jpg', 0, 0, 0) ;
-Let image11 = ('pen', './img/pen.jpg', 0, 0, 0) ;
-Let image12 = ('pet-sweep', './img/pet-sweep.jpg', 0, 0, 0) ;
-Let image13 = ('scissors', './img/scissors.jpg', 0, 0, 0) ;
-Let image14 = ('shark', './img/shark.jpg', 0, 0, 0) ;
-Let image15 = ('sweep', './img/sweep.png', 0, 0, 0) ;
-Let image16 = ('tauntaun', './img/tauntaun.jpg', 0, 0, 0) ;
-Let image17 = ('unicorn', './img/unicorn.jpg', 0, 0, 0) ;
-Let image18 = ('usb', './img/usb.gif', 0, 0, 0) ;
-Let image19 = ('water-can', './img/water-can.jpg', 0, 0, 0) ;
-Let image20 = ('wine-glass', './img/wine-glass.jpg', 0, 0, 0) ;
+let image01 = ('bag', './img/bag.jpg', 0, 0, 0) ; 
+let image02 = ('banana', './img/banana.jpg', 0, 0, 0) ; 
+let image03 = ('bathroom', './img/bathroom.jpg', 0, 0, 0) ;
+let image04 = ('boots', './img/boots.jpg', 0, 0, 0) ;
+let image05 = ('breakfast', './img/breakfast.jpg', 0, 0, 0) ;
+let image06 = ('bubblegum', './img/bubblegum.jpg', 0, 0, 0) ;
+let image07 = ('chair', './img/chair.jpg', 0, 0, 0) ;
+let image08 = ('cthulhu', './img/cthulhu.jpg', 0, 0, 0) ;
+let image09 = ('dog-duck', './img/dog-duck.jpg', 0, 0, 0) ;
+let image10 = ('dragon', './img/dragon.jpg', 0, 0, 0) ;
+let image11 = ('pen', './img/pen.jpg', 0, 0, 0) ;
+let image12 = ('pet-sweep', './img/pet-sweep.jpg', 0, 0, 0) ;
+let image13 = ('scissors', './img/scissors.jpg', 0, 0, 0) ;
+let image14 = ('shark', './img/shark.jpg', 0, 0, 0) ;
+let image15 = ('sweep', './img/sweep.png', 0, 0, 0) ;
+let image16 = ('tauntaun', './img/tauntaun.jpg', 0, 0, 0) ;
+let image17 = ('unicorn', './img/unicorn.jpg', 0, 0, 0) ;
+let image18 = ('usb', './img/usb.gif', 0, 0, 0) ;
+let image19 = ('water-can', './img/water-can.jpg', 0, 0, 0) ;
+let image20 = ('wine-glass', './img/wine-glass.jpg', 0, 0, 0) ;
 
 // push images to imageArray
 imageArray.push (image01, image02,image03)
 // imageArray.push (image01, image02, “image03”, “image04”, “image05”, “image06”, “image07”, “image08”, “image09”, “image10”, “image11”, “image12”, “image13”, “image14”, “image15”, “image16”, “image17”, “image18”, “image19”, “image20”) ;
 
 // come up with three random numbers (create function to do it)
-Let randomImages = function() {
+let randomImages = function() {
 // for loop because we need three
-	For (i = 0, i < 3, i++) {
+	For (i = 0, i < 3, i++) 
 // assign random number
-		Let randomIndex = Math.floor(Math.random() * imageArray.length) ;
+		let randomIndex = Math.floor(Math.random() * imageArray.length) ;
 // use the random number to retrieve the corresponding image
-		imageIndexArray[i] = imageArray[randomIndex]
+		imageIndexArray[i] = imageArray[randomIndex] ;
 	{
 // if / else statement to determine no duplicate images
-	If (imageIndexArray[0] != imageIndexArray[1] && 
-   imageIndexArray[0] != imageIndexArray[2] &&
-   imageIndexArray[1] != imageIndexArray[2]) {
+	If (imageIndexArray[0] != imageIndexArray[1] && imageIndexArray[0] != imageIndexArray[2] && imageIndexArray[1] != imageIndexArray[2]) 
 // if > yes: assign the src to the image
-elImage1.src = imageIndexArray[0].filePath ;
-elImage2.src = imageIndexArray[1].filePath ;
-elImage3.src = imageIndexArray[2].filePath ;
-// if > no (else): otherwise, recalculate all the values (although better would be to just recalculate the dupe)
-} else {
-break
-}
+    elImage1.src = imageIndexArray[0].filePath ;
+    elImage2.src = imageIndexArray[1].filePath ;
+    elImage3.src = imageIndexArray[2].filePath ;
+    Else  // if > no (else): otherwise, recalculate all the values (although better would be to just recalculate the dupe)
+        break
+} };
 
 // event handler #1 – count how many times an image has been displayed
-Let imageDisplayCount = function(e){
-	For (i = 0, i < imageIndexArray.length, i++){
+let imageDisplayCount = function(e){
+	For (i = 0, i < imageIndexArray.length, i++) 
 		imageIndexArray[i] += imageIndex.clickNo ;
-	{
-{
+    }
 
 // event handler #2 – count how many times an image has been clicked
-Let imageClick = function(e) {
+let imageClick = function(e) {
 	imageIndex.clickYes += 1 ;
 	imageIndex.clickNo -= 1 ;
-{
+}
 
 // attach Event Listeners to image tags
-elImage1.addEventListener(‘load’, imageDisplayCount) ;
-elImage2.addEventListener(‘load’, imageDisplayCount) ;
-elImage3.addEventListener(‘load’, imageDisplayCount) ;
+elImage1.addEventListener('load', imageDisplayCount) ;
+elImage2.addEventListener('load', imageDisplayCount) ;
+elImage3.addEventListener('load', imageDisplayCount) ;
 
-elImage1.addEventListener(‘click’, imageClick) ;
-elImage2.addEventListener(‘click’, imageClick) ;
-elImage3.addEventListener(‘click’, imageClick) ;
+elImage1.addEventListener('click', imageClick) ;
+elImage2.addEventListener('click', imageClick) ;
+elImage3.addEventListener('click', imageClick) ;
 
 // call function
 randomImages() ;
